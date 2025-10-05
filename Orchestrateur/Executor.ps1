97a1ebe240b2e1432753eb5ca872c70ba69fa654
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-10-05 16:17:55 -0400

    Executor -> Done ; Needs testing from Orchestrateur.py

--- Orchestrateur/Executor.ps1
+++ Orchestrateur/Executor.ps1
@@ -11,27 +11,35 @@ https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility
 
 #>
 
-function Install($installation_state){
+$DurationField = "Duration"
+$ResultField = "Result"
+$MachineField = "Machine"
+$SoftwareField = "Logiciel"
+
+function Install($installationState){
 	$delay = Get-Random -Minimum $DelayMin -Maximum $DelayMax
 	Start-Sleep -Seconds $delay
-	$installation_state["Duration"] = $delay
-	$installation_state["Result"] = $true, $false | Get-Random
+	$installationState[$DurationField] = $delay
+	$installationState[$ResultField] = $true, $false | Get-Random
+}
+
+function InstallationJson( $InstallationState ){
+	$JsonStr = $InstallationState | ConvertTo-Json -Depth 4
+	return $JsonStr
 }
 
 function Main(
 	[string]$computer,
 	[string]$program
 ){
-	$installation_state = @{
-		"Machine" = $computer;
-		"Logiciel" = $program;
-		"Duration" = -1;
-		"Result" = $false
+	$installationState = @{
+		$MachineField = $computer;
+		$SoftwareField = $program;
+		$DurationField = -1;
+		$ResultField = $false
 	}
-	Install $installation_state
-	Write-Host $installation_state["Duration"]
-	Write-Host $installation_state["Result"]
-	exit(0)
+	Install $installationState
+	return (InstallationJson $installationState)
 }
 
 Main $machine $logiciel
