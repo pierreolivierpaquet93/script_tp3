11794083eadbdb57eaceaff27c7e7f98c6015a3f
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-10-09 00:28:51 -0400

    Added different frames to the window ; Tests to be made

--- GestionInventaireUI/GestionInventaireUI.py
+++ GestionInventaireUI/GestionInventaireUI.py
@@ -6,8 +6,8 @@ MY_INVENTORY_TITLE = "Enonce 04 - Pierre Olivier Paquet"
 MY_INVENTORY_HEADING = "Gestion de l'inventaire"
 HEADING_FONT = "Arial"
 
-APP_WINDOW_WIDTH = "800"
-APP_WINDOW_HEIGHT = "600"
+APP_WINDOW_WIDTH = "750"
+APP_WINDOW_HEIGHT = "500"
 APP_WINDOW_RES = f"{APP_WINDOW_WIDTH}x{APP_WINDOW_HEIGHT}"
 
 # ------------------------------------------------------------------[ CLASSE.S ]
@@ -23,12 +23,16 @@ class MyInventoryApp( tkinter.Tk ):
 
 	# ----------------------------------------------------------------------
 	def FrameLayout( self ):
-		self._left_frame = tkinter.Frame( self, bg="#FF0000" )
-		self._left_frame.place(relx=0,rely=0,height=30,relwidth=0.50)
-		self._test_frame = tkinter.Frame( self, bg="#FFD900" )
-		self._test_frame.pack()
-		self.test_label = tkinter.Label(self._left_frame , text=MY_INVENTORY_HEADING )
-		self.test_label.pack(side="left")
+		top_height = 60
+		mid_height = 300
+		self._left_top_frame = tkinter.Frame( self, bg="#FF0000" )
+		self._left_top_frame.place(x=0,y=0,height=top_height,relwidth=0.50)
+		self._left_mid_frame = tkinter.Frame( self, bg="#FF9900" )
+		self._left_mid_frame.place( x=0, y=0+top_height,height=mid_height,relwidth=0.50 )
+		self._left_bot_frame = tkinter.Frame( self, bg="#D9FF00" )
+		self._left_bot_frame.place( x=0, y=0+top_height+mid_height,height=int(APP_WINDOW_HEIGHT)-top_height-mid_height,relwidth=0.50   )
+		self._right_frame = tkinter.Frame( self, bg="#00FFD5" )
+		self._right_frame.place( relx=0.5, y=0, heigh=APP_WINDOW_HEIGHT, relwidth=0.5 )
 
 	# ----------------------------------------------------------------------
 	
