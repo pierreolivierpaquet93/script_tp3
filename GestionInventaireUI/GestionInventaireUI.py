a2c9e2b581003b20b3d4ba3ca0cf02dd7ab77ccc
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-10-13 10:18:41 -0400

    Aded DisplaySelection()

--- GestionInventaireUI/GestionInventaireUI.py
+++ GestionInventaireUI/GestionInventaireUI.py
@@ -12,13 +12,14 @@ APP_WINDOW_HEIGHT = "500"
 APP_WINDOW_RES = f"{APP_WINDOW_WIDTH}x{APP_WINDOW_HEIGHT}"
 
 PRODUCT_TYPES = [
+	"",
 	"Ordinateur",
 	"Écran",
 	"Clavier",
 	"Souris"
 ]
 NEW_PRODUCT_NAME = "NEW"
-NEW_PRODUCT_TYPE = ""
+NEW_PRODUCT_TYPE = PRODUCT_TYPES[0]
 
 ESC = "\033["
 BLU = f"{ESC}1;34m"
@@ -61,6 +62,23 @@ class Inventory():
 		else:
 			new_product = Product()
 			self.__products.append( new_product )
+
+	def GetByName( self, name: str = NEW_PRODUCT_NAME ) -> Product:
+		for product in self.__products:
+			if product.GetName() == name:
+				return product
+		return None
+			
+	def GetQuantity(
+			self,
+			name: str = NEW_PRODUCT_NAME,
+			type: str = NEW_PRODUCT_TYPE
+		):
+		quantity = 0
+		for product in self.__products:
+			if product.GetName() == name and product.GetType() == type:
+				quantity += 1
+		return quantity
 
 # --------------------[ MyInventoryApp ]
 
@@ -159,7 +177,7 @@ class MyInventoryApp( tkinter.Tk ):
 		self._listbox = tkinter.Listbox(
 			self._left_mid_frame,
 			selectmode=tkinter.SINGLE,
-			width=40
+			width=45
 		)
 		self._listbox.pack(
 			side=tkinter.LEFT,
@@ -173,13 +191,15 @@ class MyInventoryApp( tkinter.Tk ):
 		self._listbox_scrollbar.pack( side=tkinter.LEFT, fill=tkinter.Y )
 		self._listbox.config( yscrollcommand=self._listbox_scrollbar.set )
 		self._listbox_scrollbar.config( command=self._listbox.yview )
+		
+		self._listbox.bind( '<<ListboxSelect>>', self.__DisplaySelection )
 
 	# ----------------------------------------------------------------------
 	def Buttons( self ):
 		self._button_add_product = tkinter.Button(
 			self._left_bot_frame,
 			text="Ajouter Produit",
-			command=self.AddProduct
+			command=self.__AddProduct
 		)
 		self._button_add_product.pack(
 			side=tkinter.LEFT,
@@ -222,8 +242,10 @@ class MyInventoryApp( tkinter.Tk ):
 
 	def Entry( self ):
 		self._entry_name = tkinter.Entry( self._right_top_info_frame )
+		self._entry_name.config( state=tkinter.DISABLED )
 		self._entry_name.place( rely=0.05, relx=0.3, relwidth=0.70 )
 		self._entry_quantity = tkinter.Entry( self._right_bot_info_frame )
+		self._entry_quantity.config( state=tkinter.DISABLED )
 		self._entry_quantity.place( rely=0.05, relx=0.3, relwidth=0.70 )
 
 	# ----------------------------------------------------------------------
@@ -231,16 +253,56 @@ class MyInventoryApp( tkinter.Tk ):
 		self._combobox = ttk.Combobox(
 			self._right_mid_info_frame,
 			values=PRODUCT_TYPES,
-			state="readonly"
+			state=tkinter.DISABLED
 		)
 		self._combobox.current(0)
 		self._combobox.place( rely=0.05, relx=0.3, relwidth=0.40 )
 
-	def AddProduct( self ):
+	# ---------------------------------------------------------------------
+
+	def __AddProduct( self ):
+		if self.inventory.GetByName():
+			print( "ERROR: the NEW product already exists!" )
+			return
 		self.inventory.AddProduct( Product() )
 		self.__log( "New Product has been added to the inventory." )
 		self._listbox.insert( tkinter.END, NEW_PRODUCT_NAME )
 		self.__log( "'NEW' product has been added to the listbox." )
+
+	# ---------------------------------------------------------------------
+	def __DisplaySelection( self, event ):
+		widget = event.widget
+		selection = widget.curselection()
+		if selection:
+			product_name = widget.get( selection[0] )
+			product = self.inventory.GetByName( product_name )
+			if product:
+				product_type = product.GetType()
+
+				self._entry_name.config( state=tkinter.NORMAL )
+				self._entry_name.delete( 0, tkinter.END )
+				self._entry_name.insert( 0, product_name )
+
+				i_type = 0
+				for type in PRODUCT_TYPES:
+					if product_type == type:
+						self._combobox.config( state='readonly' )
+						self._combobox.current(i_type)
+						break
+					i_type += 1
+
+				self._entry_quantity.config( state=tkinter.NORMAL )
+				self._entry_quantity.delete( 0, tkinter.END )
+				self._entry_quantity.insert(
+					0,
+					self.inventory.GetQuantity(
+						product_name,
+						product_type
+					)
+				)
+		else:
+			self._entry_name.delete( 0, tkinter.END )
+			self._entry_name.config( state=tkinter.DISABLED )
 
 
 # ----------------------------------------------------------------------[ MAIN ]
